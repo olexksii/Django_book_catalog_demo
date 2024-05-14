d37bc2fdf9788e01bf086b309c7c1bf9a2ffb1d4
webexpert <webalpha128@gmail.com>
2024-05-13 21:44:31 -0500

    feat(working with forms): feat view.py

--- catalog/views.py
+++ catalog/views.py
@@ -63,3 +63,37 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
         )
 
 # Create your views here.
+
+
+import datetime
+
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+
+        form = RenewBookForm(request.POST)
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+        
+        else:
+            proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+            form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+        context = {
+            'form': form,
+            'book_instance': book_instance,
+        }
+
+        return render(request, 'catalog/book_renewlibrarian.html', context)
